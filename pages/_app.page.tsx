bc9fddfba9c667eb8e73dcab01ae730294e6cf8d
Nallive Trujillo <nallive14@gmai.com>
2023-09-05 20:55:40 -0500

    Se hacen algunos cambios para el deploy

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,7 +1,7 @@
-import type { AppProps } from 'next/app'
 import {CssBaseline, ThemeProvider} from "@mui/material";
 import LayoutGeneral from "dh-marvel/components/layouts/layout-general";
 import {theme} from "dh-marvel/styles/material-theme";
+import { AppProps } from "next/dist/shared/lib/router/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <ThemeProvider theme={theme}>
